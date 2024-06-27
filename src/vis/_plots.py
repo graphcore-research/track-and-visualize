980459d41b42e79815d106791ee6f7eaff101afc
Colm Brandon <colmb@graphcore.ai>
2024-06-27 17:57:57 +0100

    added some docs

--- src/vis/_plots.py
+++ src/vis/_plots.py
@@ -12,9 +12,22 @@ def global_view(mv: MasterView,
                 title: Optional[str] = None,
                 figsize: Tuple[int, int] = (20, 10)):
     """
-    
-    
-    
+    Generates a heatmap of some scalar metric (w.r.t to the chosen Tensor Type) for each layer.
+        Y-axis:  Layer Name
+        X-axis:  Time Step
+
+
+    Args:
+        mv (MasterView): the MV you wish to visualise
+        title (str): Optional Plot title, 
+        inc (int): the increment between training iterations to include
+        figsize (Tuple[int, int]): Tuple of width, height for the size you want the plot to be
+       
+
+    Returns:
+        Figure: A Heatmap of the provided MasterView
+
+
     """
     with _plt.ioff():
         fig, axs = _plt.subplots(ncols=1, nrows=1, figsize=figsize)
